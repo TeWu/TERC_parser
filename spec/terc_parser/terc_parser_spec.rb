8af9f57f1c347a5f63f3dce7430a59fcc1fb897f
Tomasz Więch <wm.tewu@gmail.com>
2016-10-25 23:29:27 +0200

    Removed fragile test

--- spec/terc_parser/terc_parser_spec.rb
+++ spec/terc_parser/terc_parser_spec.rb
@@ -6,12 +6,6 @@ TERC_XML_FILE_PATH = File.expand_path('../../../vendor/TERC.xml', __FILE__)
 describe TercParser do
   let(:terc) { TercParser::parse_terc TERC_XML_FILE_PATH }
 
-  it "should collect correct amount of województwo, powiat and gmina" do
-    terc[:wojewodztwa].count.should == 16
-    terc[:powiaty].count.should == 379
-    terc[:gminy].count.should == 2479
-  end
-
   it "should collect data that are correct type" do
     terc.should be_instance_of Hash
     terc[:stan_na].class.should == String
